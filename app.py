51a5016b7149d41f532b6e5e818865cd4d0b33ff
Tushar Prabhu <62821534+tushar-prabhu@users.noreply.github.com>
2022-12-06 19:45:42 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+
+app = Flask(__name__)
+
+# bot = ChatBot("Candice")
+# bot.set_trainer(ListTrainer)
+# bot.set_trainer(ChatterBotCorpusTrainer)
+# bot.train("chatterbot.corpus.english")
+
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.english")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
